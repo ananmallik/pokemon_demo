51b49d2890d4a586c8630d460ce12b0ba61797cd
alanskimp80 <68741616+alanskimp80@users.noreply.github.com>
2023-04-16 12:01:52 +0100

    Update

--- PokemonDemo/PokemonDemo/DetailViewController.swift
+++ PokemonDemo/PokemonDemo/DetailViewController.swift
@@ -10,23 +10,82 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameDetail: UILabel!
+    @IBOutlet weak var heightDetail: UILabel!
+    @IBOutlet weak var weightDetail: UILabel!
+    @IBOutlet weak var orderDetail: UILabel!
+    @IBOutlet weak var baseExperienceDetail: UILabel!
+    
     var detailURL = ""
     
+    //Label variables:
+    var name = ""
+    var height = 0
+    var weight = 0
+    var order = 0
+    var base_experience = 0
+     
+
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
         setLabels()
+        
+        //Set labels with values:
+        getdetailData(){
+            self.nameDetail.text = "Name: " + self.name.capitalized
+            self.heightDetail.text = "Height: " + String(self.height)
+            self.weightDetail.text = "Weight: " + String(self.weight)
+            self.orderDetail.text = "Order: " + String(self.order)
+            self.baseExperienceDetail.text = "Base Experience: " + String(self.base_experience)
+        }
+
     
     }
     
     func setLabels() {
         
         let defaults = UserDefaults.standard
-        
         self.nameLabel.text = defaults.object(forKey:"name") as? String
         self.detailURL = defaults.string(forKey:"url")!
         
     }
 
+    func getdetailData(completed: @escaping () -> ()) {
+        
+        if let url = URL(string: self.detailURL) {
+            
+            //print (self.detailURL)
+            
+           URLSession.shared.dataTask(with: url) { data, response, error in
+              if let data = data {
+                 if let jsonString = String(data: data, encoding: .utf8) {
+                    let data = Data(jsonString.utf8)
+
+                    do {
+                        // make sure this JSON is in the format we expect
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            // try to read out a string array
+                            if true {
+                                
+                                self.name = json["name"]! as! String
+                                self.height = json["height"]! as! Int
+                                self.weight = json["weight"]! as! Int
+                                self.order = json["order"]! as! Int
+                                self.base_experience = json["base_experience"]! as! Int
+                                
+                                DispatchQueue.main.async {
+                                    completed()
+                                }
+                            }
+                        }
+                    } catch let error as NSError {
+                        print("Failed to load: \(error.localizedDescription)")
+                    }
+                 }
+               }
+           }.resume()
+        }
+
+    }
 }
